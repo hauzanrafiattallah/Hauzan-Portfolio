fcff8bf17babe77e599b2eb9e5b6390e801e8d77
Hauzan Rafi Attallah <rafihauzan42@gmail.com>
2024-08-28 21:13:17 +0700

    chore: fix import casing in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import About from "./components/About";
 import Contact from "./components/Contact";
 import Experience from "./components/Experience";
 import Hero from "./components/Hero";
-import Navbar from "./components/navbar";
+import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
 import Technologies from "./components/Technologies";
 
